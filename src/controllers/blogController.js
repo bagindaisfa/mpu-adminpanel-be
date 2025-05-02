1c3475f164dfc6906fd6092fcd24b49bdfe317e0
bagindaQuadrant <bpulungan@appen.com>
2025-05-02 14:11:26 +0700

    Update blogController.js

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -42,10 +42,16 @@ const getBlogById = async (req, res) => {
   const { id } = req.params;
   try {
     const result = await pool.query('SELECT * FROM blogs WHERE id = $1', [id]);
+
+    const baseUrl = process.env.BASE_URL;
+    const blogsWithImages = result.rows.map((blog) => ({
+      ...blog,
+      image_path: `${baseUrl}/${blog.image_path}`, // Path disesuaikan
+    }));
     if (result.rows.length === 0) {
       return res.status(404).json({ message: 'Blog not found' });
     }
-    res.json(result.rows[0]);
+    res.json(blogsWithImages[0]);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Failed to fetch blog' });
